74f7fe3f410c41219e604d81f94e434945f36fa6
dan1e11s <danielmuratov2003@gmail.com>
2023-04-13 21:26:59 +0600

    first commit

---
+++ src/common/buttons/Button/Button.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import './Button.css';
+
+interface ButtonProps extends React.HTMLProps<HTMLButtonElement> {}
+
+export const Button: React.FC<ButtonProps> = ({ children }) => {
+  return <button>{children}</button>;
+};
